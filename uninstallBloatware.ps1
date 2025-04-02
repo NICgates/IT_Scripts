44a0096716d74f48f78123acde8d194996be9c15
NICgates <39223425+NICgates@users.noreply.github.com>
2025-04-01 18:14:19 -0700

    Update uninstallBloatware.ps1

--- uninstallBloatware.ps1
+++ uninstallBloatware.ps1
@@ -3,18 +3,57 @@
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned
 
 $appList = @(
+    # MICROSOFT APPS
+    # "Microsoft.WindowsStore*"
+    # "Microsoft.StorePurchaseApp*"
     "Microsoft.Copilot*"
     "Microsoft.SkypeApp*"
     "Microsoft.YourPhone*"
-    "king.com.CandyCrushFriends*"
-    "king.com.FarmHeroesSaga*"
-    "FINGERSOFT.HILLCLIMBRACING*"
+    "Microsoft.BingWeather*"
+    "Microsoft.GetHelp*"
+    "Microsoft.MixedReality.Portal*"
+    "Microsoft.People*"
+    "Microsoft.Print3D*"
+    "Microsoft.SkypeApp*"
+    "Microsoft.Wallet*"
+    "Microsoft.WindowsAlarms*"
+    "Microsoft.windowscommunicationsapps*"
+    "Microsoft.WindowsFeedbackHub*"
+    "Microsoft.WindowsMaps*"
+    "Microsoft.YourPhone*"
+    "Microsoft.ZuneMusic*"
+    "Microsoft.ZuneVideo*"
+    "*ActiproSoftwareLLC*"
+    
+    # GAMES
+    "*CandyCrush*"
+    "*king.com.FarmHeroesSaga*"
+    "*HILLCLIMBRACING*"
     "Microsoft.MicrosoftSolitaireCollection*"
-    "Microsoft.Xbox*"
+    "Microsoft.Maps*"
+    "Microsoft.Microsoft3DViewer*"
+    "*ROBLOXCORPORATION.ROBLOX*"
+    "*HiddenCityMysteryofShadows*"
+    
+    # XBOX APPS
+    "Microsoft.Xbox.TCUI*"
+    "Microsoft.XboxApp*"
+    "Microsoft.XboxGameOverlay*"
+    "Microsoft.XboxGamingOverlay*"
+    "Microsoft.XboxIdentityProvider*"
+    "Microsoft.XboxSpeechToTextOverlay*" 
 )
 
 foreach ($app in $appList) {
     Get-AppxPackage -allusers $app | Remove-AppxPackage -ErrorAction SilentlyContinue               
 }
 
-Set-ExecutionPolicy -ExecutionPolicy Restricted
+
+   
+
+ 
+    
+    
+
+
+
